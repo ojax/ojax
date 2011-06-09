081f54750c903e6e133b12b19d8f5b3b12d227ca
Dave Jeffery <dave@davejeffery.com>
2011-06-09 16:42:13 +0100

    Adding admin filter controls

--- apps/activity/admin.py
+++ apps/activity/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from activity.models import Activity, ActivityComment
 
+class AuthorAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created'
+    list_filter = ('source', 'user')
 
-admin.site.register(Activity)
+admin.site.register(Activity, ActivityAdmin)
 admin.site.register(ActivityComment)
